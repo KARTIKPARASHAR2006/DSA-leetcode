06a8ff986562b039e0b513ebf26e7a5a2ae699d9
KARTIKPARASHAR2006 <parashar.kartik2006@gmail.com>
2026-08-04 08:46:56 +0530

    Time: 8 ms (12.18%) | Memory: 33.8 MB (8.08%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        if (nums.empty()) return {};
-        
-        sort(nums.begin(), nums.end());  
-        
-        vector<int> result;
-        int expected = nums[0];
-        
-        for (int num : nums) {
-            while (expected < num) {
-                result.push_back(expected);
-                expected++;
-            }
-            expected = num + 1;
+        sort(nums.begin(), nums.end());
+        vector<int> ans;
+        int n = nums.size();
+
+        int start = nums[0];
+        int end = nums[n-1];
+
+        unordered_map<int, int> mpp;
+        for(int i = 0; i<n; i++){
+            mpp[nums[i]];
         }
-        
-        return result;
+
+        for(int i = start; i<=end; i++){
+            if(mpp.find(i) == mpp.end()){
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
     }
 };
